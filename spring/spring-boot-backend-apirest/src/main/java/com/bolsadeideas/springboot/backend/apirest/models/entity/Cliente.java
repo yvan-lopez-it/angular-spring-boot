af85edf7220535234640fbc75902fa69e98c81a7
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-02-20 13:33:59 -0500

    class 37

---
+++ spring/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
+
+
