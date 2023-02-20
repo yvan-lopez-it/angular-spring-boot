1024bfc08016d392c4d6bd289d1e0e4f61cf6389
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-02-20 12:21:47 -0500

    add files

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Yvan', apellido: 'Lopez', email: 'yvan.lopez@mail.com', createdAt: '2023-01-19' },
+  { id: 2, nombre: 'Benjamin', apellido: 'Rivera', email: 'user2@mail.com', createdAt: '2023-02-14' },
+  { id: 3, nombre: 'Maritza', apellido: 'Tiburcio', email: 'user3@mail.com', createdAt: '2023-03-15' },
+  { id: 4, nombre: 'Fausto', apellido: 'De La Cruz', email: 'user4@mail.com', createdAt: '2023-04-26' },
+  { id: 5, nombre: 'Jose', apellido: 'Sainz', email: 'user5@mail.com', createdAt: '2023-05-25' },
+  { id: 6, nombre: 'Eber', apellido: 'Oviedo', email: 'user6@mail.com', createdAt: '2023-06-24' },
+  { id: 7, nombre: 'Silvana', apellido: 'Ramirez', email: 'user7@mail.com', createdAt: '2027-07-23' },
+  { id: 8, nombre: 'Jose', apellido: 'Carrasco', email: 'user8@mail.com', createdAt: '2023-08-22' },
+  { id: 9, nombre: 'Carlos', apellido: 'Guanilo', email: 'user9@mail.com', createdAt: '2023-09-21' },
+  { id: 10, nombre: 'Antonio', apellido: 'Botto', email: 'user10@mail.com', createdAt: '2023-10-20' },
+];
