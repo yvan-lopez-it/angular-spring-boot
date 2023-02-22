280a25c24ebe0710075719114cee2159f48029f4
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-02-22 17:33:44 -0500

    termino de la seccion 5

---
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from "./cliente";
+import { ClienteService } from "./cliente.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+})
+export class FormComponent implements OnInit {
+
+  public cliente: Cliente = new Cliente();
+  public titulo: string = 'Crear cliente';
+
+  constructor(private clienteService: ClienteService, private router: Router, private activateRoute: ActivatedRoute,) {
+  }
+
+  ngOnInit(): void {
+    this.cargarCliente();
+  }
+
+  cargarCliente(): void {
+    this.activateRoute.params.subscribe(
+      params => {
+        let id = params['id']
+        if (id) {
+          this.clienteService.getCliente(id).subscribe(
+            cliente => this.cliente = cliente
+          );
+        }
+      }
+    );
+  }
+
+  create(): void {
+    this.clienteService.create(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate([ '/clientes' ]);
+        swal.fire('Nuevo Cliente', `Cliente ${ cliente.nombre } creado con éxito!`, 'success');
+      }
+    );
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate([ '/clientes' ]);
+        swal.fire('Cliente actualizado', `Cliente ${ cliente.nombre } actualizado con éxito!`, 'success');
+      }
+    );
+  }
+}
