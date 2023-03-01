33eb0eef9041496cd4a36cc4ab4714d607473239
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-03-01 02:06:11 -0500

    Clase 83

--- spring/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -1,12 +1,16 @@
 package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IClienteService {
 
     List<Cliente> findAll();
+
+    Page<Cliente> findAll(Pageable pageable);
 
     Cliente findById(Long id);
 
